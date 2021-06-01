6031288b0041b57e17c45bad353694a18b4e3c90
Quanisha Williams <ms_q_williams@gmail.com>
2021-06-01 17:26:45 +0000

    Initial Commit

---
+++ Game.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Game {
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Howdy, what's your name?");
+    String name = input.nextLine();
+    
+
+  
+  }
+
+}
